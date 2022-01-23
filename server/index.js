9b026c7a3700238b7cf1126430f1b2651f251e14
Rituparna Warwatkar <rituparnaw16@gmail.com>
2022-01-23 20:26:54 +0530

    reorganized socket.io

--- server/index.js
+++ server/index.js
@@ -1,44 +1,21 @@
 const express = require('express');
-const cors=require('cors')
+const cors = require('cors');
 const app = express();
-const http = require("http")
-const server = http.Server(app)
+
 const DB = require('./src/utils/db');
 const api = require('./api');
 const auth = require('./src/middlewares/auth');
 const PORT = process.env.PORT || 5000;
 
-const io = require("socket.io")(server, {
-	cors: {
-		origin: "http://localhost:3000",
-		methods: [ "GET", "POST" ],
-    credentials:true
-	}
-});
-app.use(cors())
-app.options('*',cors())
-app.get('/cors',(req,res)=>{
-  res.set('Access-Control-Allow-Origin',"http://localhost:3000");
+app.use(cors());
+app.options('*', cors());
+app.get('/cors', (req, res) => {
+  res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.send({
-    msg:"This has CORS enabled"
-  })
+    msg: 'This has CORS enabled',
+  });
 });
 
-io.on("connection", (socket) => {
-	socket.emit("me", socket.id)
-
-	socket.on("disconnect", () => {
-		socket.broadcast.emit("callEnded")
-	})
-
-	socket.on("callUser", (data) => {
-		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
-	})
-
-	socket.on("answerCall", (data) => {
-		io.to(data.to).emit("callAccepted", data.signal)
-	})
-})
 app.use(express.json());
 
 app.use('/api/v1', api);
@@ -56,19 +33,27 @@ app.use((err, req, res, next) => {
     data: err.data,
   });
 });
-server.listen(PORT,()=>console.log("server started"))
-/*DB()
+// server.listen(PORT, () => console.log('server started'));
+
+let io;
+
+DB()
   .then(() => {
     console.log('Db connected');
     return app.listen(PORT);
   })
-  .then(() => {
-    console.log('serving',PORT);
+  .then((server) => {
+    io = require('socket.io')(server, {
+      cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+      },
+    });
+    console.log('serving', PORT);
   })
   .catch((err) => {
     console.log(err.message);
     process.exit(0);
-  });*/
+  });
 
-
-  
+module.exports = io;
---
+++ server/src/socket.js
@@ -0,0 +1,21 @@
+const io = require('./../index');
+
+io.on('connection', (socket) => {
+  socket.emit('me', socket.id);
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('callEnded');
+  });
+
+  socket.on('callUser', (data) => {
+    io.to(data.userToCall).emit('callUser', {
+      signal: data.signalData,
+      from: data.from,
+      name: data.name,
+    });
+  });
+
+  socket.on('answerCall', (data) => {
+    io.to(data.to).emit('callAccepted', data.signal);
+  });
+});
